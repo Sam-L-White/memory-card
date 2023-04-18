107a620b0a66d889c9859e0ce9d6e25a0d0c39d1
Sam White <samuelwhite98@outlook.com>
2023-04-18 19:29:52 +0100

    Add score styling

--- src/components/Scoreboard.js
+++ src/components/Scoreboard.js
@@ -3,9 +3,15 @@ import React, {useState, useEffect} from "react";
 const Scoreboard = (props) => {
 
     return(
-        <div>
-            <div>{props.score}</div>
-            <div>{props.highScore}</div>
+        <div className="flex flex-row gap-10">
+            <div className="flex flex-row gap-5 text-white text-4xl bg-green-800 drop-shadow-[0_5px_3.5px_rgba(0,0,0,0.8)] p-5">
+                <div className="drop-shadow-[0_5px_3.5px_rgba(0,0,0,0.8)]">Score:</div>
+                <div className="drop-shadow-[0_5px_3.5px_rgba(0,0,0,0.8)]">{props.score}</div>
+            </div>
+            <div className="flex flex-row gap-5 text-white text-4xl bg-green-800 drop-shadow-[0_5px_3.5px_rgba(0,0,0,0.8)] p-5">
+                <div className="drop-shadow-[0_5px_3.5px_rgba(0,0,0,0.8)]">High Score:</div>
+                <div className="drop-shadow-[0_5px_3.5px_rgba(0,0,0,0.8)]">{props.highScore}</div>
+            </div>
         </div>
     )
 }
